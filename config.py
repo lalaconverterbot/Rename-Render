c8c375831aa5f87f0b1c63c957ae7dd4c304c53d
lalaconverterbot <145864064+lalaconverterbot@users.noreply.github.com>
2024-02-18 02:33:15 -0800

    Wizard

--- config.py
+++ config.py
@@ -1,25 +1,24 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
+# Don't Remove Credit @flstudiofruity
+# Subscribe YouTube Channel For Amazing Bot @flstudiofruity
+# Ask Doubt on telegram @flstudiofruity
 
 import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
+API_ID = os.environ.get("API_ID", "14084834")
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "3370f5b5ed3ac5ad6931724413f529f6")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6569891735:AAGmrfy-CF3_scVWMPV8OSwwvWImbCAPihM") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "VJ_Botz") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "flstudiofruity") 
 
-             # Don't Remove Credit @VJ_Botz
-             # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-             # Ask Doubt on telegram @KingVJ01
+             # Don't Remove Credit @flstudiofruity
+             # Subscribe YouTube Channel For Amazing Bot @flstudiofruity
+             # Ask Doubt on telegram @flstudiofruity
 
-DB_NAME = os.environ.get("DB_NAME", "renamevjbot")     
+DB_NAME = os.environ.get("DB_NAME", "filewizardprobot")     
 
 DB_URL = os.environ.get("DB_URL", "")
  
@@ -27,10 +26,10 @@ FLOOD = int(os.environ.get("FLOOD", "10"))
 
 START_PIC = os.environ.get("START_PIC", "https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5606411877').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1602317498').split()]
 
 PORT = os.environ.get("PORT", "8080")
 
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @flstudiofruity
+# Subscribe YouTube Channel For Amazing Bot @flstudiofruity
+# Ask Doubt on telegram @flstudiofruity
